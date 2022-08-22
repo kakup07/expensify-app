1f134b908844be24b8fdb6bbf9ea2a56f6d607b0
kakup07 <pandey.kaku@live.com>
2022-08-22 17:14:26 +0530

    express

--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
+const path = require('path');
 const express = require('express');
-const path = require('path')
 const app = express();
-
-// const publicPath = path.join(__dirname, '..', 'public')
-// app.use(express.static(publicPath));
-
-app.get('/api', (req, res) => {
-    res.json({message: "Hello from Server!"})
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
 });
-
-app.listen(process.env.PORT || 3001, () => {
-    console.log('server is up!')
+app.listen(port, () => {
+   console.log('Server is up!');
 });
